d74002c2bb05f7f988facf230e7834e69b0e24ae
Carlos-Teixeira-Jr <teixeirahist1988@gmail.com>
2022-08-12 13:37:23 -0300

    finalização do projeto

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Respostas");
+
+//Database
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Conexão feita com o banco de dados")
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  })
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine','ejs');
@@ -11,7 +24,13 @@ app.use(bodyParser.json());
 
 //Rotas
 app.get("/",(req, res) => {
-  res.render("index");
+  Pergunta.findAll({ raw: true, order:[
+    ['id', 'DESC']
+  ]}).then(perguntas => {
+    res.render("index", {
+      perguntas: perguntas
+    });
+  });
 });
 
 app.get("/perguntar",(req, res) => {
@@ -21,7 +40,46 @@ app.get("/perguntar",(req, res) => {
 app.post("/salvarpergunta", (req, res) =>{
   let titulo = req.body.titulo;
   let descricao = req.body.descricao;
-  res.send("Formulário recebido! titulo " + titulo + " " + " descricao " + descricao);
+  Pergunta.create({
+    titulo: titulo,
+    descricao: descricao
+  }).then(() => {
+    res.redirect("/");
+  })
+});
+
+app.get("/pergunta/:id", (req, res) =>{
+  var id = req.params.id;
+  Pergunta.findOne({
+    where: {id: id},
+  }).then(pergunta => {
+    if(pergunta != undefined){//pergunta encontrada
+      Resposta.findAll({
+        where: {perguntaId: pergunta.id},
+        order: [
+          ['id', 'DESC']
+        ]
+      }).then(respostas => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas
+        });
+      });
+    }else{ //pergunta não encontrada = undefined
+      res.redirect("/");
+    }
+  })
+})
+
+app.post("/responder", (req, res) =>{
+  let corpo = req.body.corpo;
+  let perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect("/pergunta/"+perguntaId);
+  });
 });
 
 app.listen(8080,()=>{console.log("App rodando!");});
